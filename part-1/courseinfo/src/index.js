d3b1a730f5954eee09ba93b32d1442dc5426e127
MAVP <localhost@debian>
2020-04-18 18:33:32 -0400

    part1-courseinfo-part1-2

--- part-1/courseinfo/src/index.js
+++ part-1/courseinfo/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => <h1>{props.course}</h1>
 
+const Part = (props) => <p>{props.part} {props.exercise}</p>
+
 const Content = (props) => {
   return (
     <>
-    <p>{props.part} {props.exercise}</p>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
     </>
   )
 }
@@ -25,9 +29,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part1={part1} exercises1={exercises1}
+               part2={part2} exercises2={exercises2}
+               part3={part3} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
