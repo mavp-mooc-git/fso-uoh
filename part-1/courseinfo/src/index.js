defafde71daa0ee0be494a297a3465edd514813b
MAVP <localhost@debian>
2020-04-18 22:07:34 -0400

    part1-courseinfo-part1-5

--- part-1/courseinfo/src/index.js
+++ part-1/courseinfo/src/index.js
@@ -20,27 +20,29 @@ const Total = (props) => <p>Number of exercises {props.total[0] +
                           props.total[2]}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  const total = parts.map(p => p.exercises)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  const total = course.parts.map(p => p.exercises)
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total total={total} />
     </div>
   )
