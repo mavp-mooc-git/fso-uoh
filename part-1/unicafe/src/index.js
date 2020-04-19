80ddfd9c876dc208c4b4bab35df6ec5e323829b3
MAVP <localhost@debian>
2020-04-19 18:33:43 -0400

    part1-unicafe-part1-7

--- part-1/unicafe/src/index.js
+++ part-1/unicafe/src/index.js
@@ -6,6 +6,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (all > 0) ? (good - bad) / all : 0
+  const positive = (all > 0) ? (good / all) * 100 : 0
 
   return (
     <div>
@@ -23,7 +26,10 @@ const App = () => {
       <p>
         good {good}<br />
         neutral {neutral}<br />
-        bad {bad}
+        bad {bad}<br />
+        all {all}<br />
+        average {average}<br />
+        positive {positive} %
       </p>
     </div>
   )
