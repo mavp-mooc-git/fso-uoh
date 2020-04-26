5ca2cc8e3474ac7e8b58a214ca0290426bfff8c1
MAVP <localhost@debian>
2020-04-26 18:10:37 -0400

    part2-phonebook-part2-6

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -6,19 +6,34 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
+      {JSON.stringify(persons)}
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(p => <p key={p.name}>{p.name}</p>)}
     </div>
   )
 }
