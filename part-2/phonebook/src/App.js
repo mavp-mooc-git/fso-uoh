ecf1ee705b1125763afb836a30fface1b0937e34
MAVP <localhost@debian>
2020-04-26 19:22:26 -0400

    part2-phonebook-part2-8

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -2,27 +2,38 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      number: newNumber
     }
     const aux = persons.map(p => p.name)
     if(aux.indexOf(newName) !== -1) {
       window.alert(`${newName} is already added to phonebook`);
+    }
+    else if(newName === "" || newNumber === "" ) {
+      window.alert("name or number fields can't be empty");
     } else {
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -33,11 +44,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <p key={p.name}>{p.name}</p>)}
+      {persons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
     </div>
   )
 }
