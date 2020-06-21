81042933b7da6759cb79ee347f56beb64bae2e27
MAVP <localhost@debian>
2020-06-21 18:57:58 -0400

    part3-17

--- part-3/phonebook-backend/models/person.js
+++ part-3/phonebook-backend/models/person.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url, {
-  useFindAndModify: false,
   useUnifiedTopology: true,
   useNewUrlParser: true
 })
