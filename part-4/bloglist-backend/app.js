aba18aa00bb523ad1ed5d9e36e1b4883e33db87b
MAVP <localhost@debian>
2020-07-17 23:41:23 -0400

    part4-18

--- part-4/bloglist-backend/app.js
+++ part-4/bloglist-backend/app.js
@@ -10,6 +10,7 @@ const blogsRouter = require('./controllers/bloglists')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 logger.info('connecting to', mongoUrl)
@@ -34,6 +35,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
