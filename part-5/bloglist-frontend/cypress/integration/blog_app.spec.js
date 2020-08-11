edbe6e600a983cb95fc92b30952f8794e6a7767f
MAVP <localhost@debian>
2020-08-11 00:32:21 -0400

    part5-18

--- part-5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part-5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -4,16 +4,41 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('front page can be opened', function() {
-    cy.contains('BLOGS')
-    cy.contains('Blog app, Full Stack Open 2020, University of Helsinki.')
+  describe('Initial page',function() {
+    it('front page can be opened', function() {
+      cy.contains('BLOGS')
+      cy.contains('Blog app, Full Stack Open 2020, University of Helsinki.')
+    })
+
+    it('Login form is shown', function() {
+      cy.contains('login').click()
+    })
   })
 
-  it('Login form is shown', function() {
-    cy.contains('login').click()
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('Matti Luukkainen logged-in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('otherpass')
+      cy.get('#login-button').click()
+    })
   })
 
 })
