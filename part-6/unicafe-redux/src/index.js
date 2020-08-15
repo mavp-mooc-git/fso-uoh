55fa38a0acd65cb1bf59e3f266335c7f81d22c92
MAVP <localhost@debian>
2020-08-15 14:08:41 -0400

    part6-2

--- part-6/unicafe-redux/src/index.js
+++ part-6/unicafe-redux/src/index.js
@@ -9,24 +9,67 @@ const pStyle = {
   fontSize: '16px'
 }
 
+const dispatch = (action) => {
+  store.dispatch({
+    type: action
+  })
+}
+
+const Header = () => <h1>give feedback</h1>
+
+const Button = ({action, text}) => {
+  return <button onClick={() => dispatch(action)}>{text}</button>
+}
+
+const Statistics = () => {
+  const good = store.getState().good
+  const ok = store.getState().ok
+  const bad = store.getState().bad
+  const all = good + ok + bad
+  const average = (all > 0) ? (good - bad) / all : 0
+  const positive = (all > 0) ? (good / all) * 100 : 0
+
+  if(all === 0) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+        <pre>
+          state: &#123;
+            good: {good},
+            neutral: {ok},
+            bad: {bad}&nbsp;
+          &#125;
+        </pre>
+      </>
+    )
+  }
+  return (
+    <>
+      <h2>statistics</h2>
+      <pre style={pStyle}>
+        good      :  {good} <br />
+        neutral   :  {ok} <br />
+        bad       :  {bad} <br />
+        all       :  {all} <br />
+        average   :  {average} <br />
+        positive  :  {positive} %
+      </pre>
+    </>
+  )
+}
+
 const App = () => {
   return (
     <div>
+      <Header />
+      <Button action='GOOD' text='good' /> &nbsp;
+      <Button action='OK' text='neutral' /> &nbsp;
+      <Button action='BAD' text='bad' /> &nbsp;
+      <Button action='ZERO' text='reset stats' /> &nbsp;
+      <Button action='OTHER' text='other actions' /> &nbsp;
       <br />
-      <button onClick={() => console.log('good')}>good</button> &nbsp;
-      <button onClick={() => console.log('neutral')}>neutral</button>  &nbsp;
-      <button onClick={() => console.log('bad')}>bad</button> &nbsp;
-      <button onClick={() => console.log('reset')}>reset stats</button>  &nbsp;
-      <button onClick={() => console.log('nothing')}>no actions</button>
-      <br />
-      <pre style={pStyle}>
-        good      =  {store.getState().good} <br />
-        neutral   =  {store.getState().ok} <br />
-        bad       =  {store.getState().bad} <br />
-        all       =  0 <br />
-        average   =  0 <br />
-        positive  =  0 %
-      </pre>
+      <Statistics />
     </div>
   )
 }
