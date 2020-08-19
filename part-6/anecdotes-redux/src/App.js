2907cbf158c2d5ecc811053bbdd5093b5f0873f7
MAVP <localhost@debian>
2020-08-19 17:47:21 -0400

    part6-12

--- part-6/anecdotes-redux/src/App.js
+++ part-6/anecdotes-redux/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Notification from './components/Notification'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Filter from './components/Filter'
 
 const App = () => {
   return (
     <div>
       <h1>Anecdotes</h1>
       <Notification />
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- part-6/anecdotes-redux/src/components/AnecdoteList.js
+++ part-6/anecdotes-redux/src/components/AnecdoteList.js
@@ -6,6 +6,7 @@ import { msgVote, deleteMessage } from '../reducers/notificationReducer'
 const AnecdoteList = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => state.anecdotes)
+  const filterData = useSelector(state => state.filter)
 
   const message = (id) => {
     const data = anecdotes.find(n => n.id === id)
@@ -15,14 +16,20 @@ const AnecdoteList = () => {
     }, 5000)
   }
 
+  const filters = anecdotes.filter(p => {
+    const e1 = p.content.toString().toLowerCase()
+    const e2 = filterData.toString().toLowerCase()
+    return e1.indexOf(e2) > -1
+  })
+
   const vote = (id) => {
-    message(id)
     dispatch(addVote(id))
+    message(id)
   }
 
   return (
     <>
-      {anecdotes.map(anecdote => anecdote)
+      {filters.map(anecdote => anecdote)
       .sort((a, b) => (a.votes > b.votes) ? -1 : 1)
       .map(anecdote =>
         <div key={anecdote.id}>
