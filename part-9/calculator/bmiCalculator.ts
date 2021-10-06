15afa24bd92d2660a469aac9f48dba25516e1e21
MAVP <localhost@debian>
2021-10-05 23:03:24 -0300

    part9-5

--- part-9/calculator/bmiCalculator.ts
+++ part-9/calculator/bmiCalculator.ts
@@ -33,28 +33,4 @@ const calculateBmi = (h: number, w: number) => {
   return result;
 };
 
-try {
-  interface Values {
-    h: number;
-    w: number;
-  }
-  
-  const parseArguments = (args: Array<string>): Values => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
-  
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-      return {
-        h: Number(args[2]),
-        w: Number(args[3])
-      }
-    } else {
-      throw new Error('Provided values were not numbers!');
-    }
-  }
-
-  const { h, w } = parseArguments(process.argv);
-  console.log(calculateBmi(h, w));
-} catch (e) {
-  console.log('Error, something bad happened, message: ', e.message);
-}
+export default calculateBmi;
--- part-9/calculator/index.ts
+++ part-9/calculator/index.ts
@@ -1,4 +1,6 @@
-import express = require('express');
+import express from 'express';
+import calculateBmi from './bmiCalculator';
+
 const app = express();
 
 app.get('/ping', (_req, res) => {
@@ -7,6 +9,26 @@ app.get('/ping', (_req, res) => {
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  try {
+    if (height === 0 || weight === 0) throw new Error('missing parameters');
+    if (isNaN(Number(height)) || isNaN(Number(weight))) throw new Error('malformatted parameters');
+    res.send({
+      weight,
+      height,
+      bmi: calculateBmi(height, weight)
+    });
+  } catch(e) {
+    res.status(400);
+    res.send({
+      error: e.message
+    });
+  }
 });
 
 const PORT = 3003;
