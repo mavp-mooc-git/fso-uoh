8029f67671595b00c79b547ec741b1a117a06064
MAVP <localhost@debian>
2021-10-08 22:57:22 -0300

    part9-10

--- part-9/patientor-backend/build/index.js
+++ part-9/patientor-backend/build/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diagnoses_1 = __importDefault(require("./routes/diagnoses"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
@@ -13,6 +14,7 @@ app.get('/api/ping', (_req, res) => {
     //console.log('someone pinged here');
     res.send('pong');
 });
+app.use('/api/diagnoses', diagnoses_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
